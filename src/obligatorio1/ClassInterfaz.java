9143c8bfde9bacd164b06176d30ad23cc45da24b
mat1ask <136759965+mat1ask@users.noreply.github.com>
2023-10-03 23:18:23 -0300

    Funcionan los colores y el formato del tablero en consola

--- src/obligatorio1/ClassInterfaz.java
+++ src/obligatorio1/ClassInterfaz.java
@@ -1,67 +1,109 @@
-
 package obligatorio1;
 
 import java.util.*;
 
 public class ClassInterfaz {
-    
-    public static void main(String[] args){
+
+    public static void menu() {
+        System.out.println("-------------------------SOLIFLIPS-------------------------");
+
         Scanner in = new Scanner(System.in);
         System.out.println("Desea Jugar?");
         String respuesta = in.nextLine();
-        TableroAzar tablero;
         int opcion = 0;
 
-        if(respuesta.toLowerCase().equals("si")){
+        if (respuesta.toLowerCase().equals("si")) {
+            System.out.println("-----------------------------------------------------------");
             System.out.println("1)Tomar datos del archivo");
             System.out.println("2)Usar el tablero predefinido");
             System.out.println("3)Usar un tablero al azar");
-            opcion = pedirEntero("\nEliga su opcion (1-3)",1,3);
-            switch(opcion){
+            opcion = pedirEntero("\nEliga su opcion (1-3)", 1, 3);
+            switch (opcion) {
                 case 1:
                     System.out.println("Seleccionaste tomar datos del archivo");
-                break;
+                    break;
                 case 2:
-                    System.out.println("Seleccionaste el tablero predefinido");
-                    char [][] predefinido = {{'|', '|', '-', '/', '|', '-'},{'-', '/', '/', '|', '-', '-'},{'-', '-', '|', '-', '/', '-'},{'\\', '-', '|', '\\', '|', '|'},{'\\', '/', '/', '|', '/', '\\'}};
-                    System.out.println(predefinido);
-                break;
+                    System.out.println("\nSeleccionaste el tablero predefinido");
+//                    String[][] predefinido = {
+//                        {" ", " ", "1", " ", "2", " ", "3", " ", "4", " ", "5", " ", "6"},
+//                        {" ", "+---+---+---+---+---+---+"},
+//                        {"1", "|", "|", "|", "|", "|", "-", "|", "/", "|", "|", "|", "-", "|"},
+//                        {" ", "+---+---+---+---+---+---+"},
+//                        {"2", "|", "-", "|", "/", "|", "/", "|", "|", "|", "-", "|", "-", "|"},
+//                        {" ", "+---+---+---+---+---+---+"},
+//                        {"3", "|", "-", "|", "-", "|", "|", "|", "-", "|", "/", "|", "-", "|"},
+//                        {" ", "+---+---+---+---+---+---+"},
+//                        {"4", "|", "\\", "|", "-", "|", "|", "|", "\\", "|", "|", "|", "|", "|"},
+//                        {" ", "+---+---+---+---+---+---+"},
+//                        {"5", "|", "\\", "|", "/", "|", "/", "|", "|", "|", "/", "|", "\\", "|"},
+//                        {" ", "+---+---+---+---+---+---+"}};
+//                    mostrarTableroPredefinido(predefinido);
+                    TableroAzar tablero = new TableroAzar();
+                    System.out.println(tablero);
+                    jugar();
+                    break;
                 case 3:
                     System.out.println("Seleccionaste el tablero al azar");
-                    int filas = pedirEntero("\nEliga la cantidad de filas del tablero",3,9);
-                    int columnas = pedirEntero("\nEliga la cantidad de columnas del tablero",3,9);
-                    int nivel = pedirEntero("\nEliga el nivel del juego",1,8);
-                    tablero = new TableroAzar(filas,columnas,nivel);
-                break;
+                    int filas = pedirEntero("\nEliga la cantidad de filas del tablero: ", 3, 9);
+                    int columnas = pedirEntero("\nEliga la cantidad de columnas del tablero: ", 3, 9);
+                    int nivel = pedirEntero("\nEliga el nivel del juego: ", 1, 8);
+                    jugar();
+                    break;
             }
-    
-        }else {
-            System.out.println("¡Hasta luego!");
+        } else {
+            System.out.println("Â¡Hasta luego!");
+        }
+
+    }
+
+    public static int pedirEntero(String mensaje, int minimo, int maximo) {
+        Scanner in = new Scanner(System.in);
+        int dato = 0;
+        boolean ok = false;
+
+        while (!ok) {
+            try {
+                System.out.print(mensaje);
+                dato = in.nextInt();
+                in.nextLine();
+                if (dato >= minimo && dato <= maximo) {
+                    ok = true;
+                } else {
+                    System.out.println("Valor fuera de rango (" + minimo + "-" + maximo + ")");
+                }
+            } catch (InputMismatchException var7) {
+                System.out.println("Ingrese solo numeros");
+                in.nextLine();
+            }
+        }
+
+        return dato;
+    }
+
+    public static void mostrarTableroPredefinido(String[][] predefinido) {
+        for (String[] row : predefinido) {
+            for (String cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
         }
     }
+
+    public static void jugar() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese numero de fila a cambiar");
+        int fila = in.nextInt();
+        System.out.println("Ingrese numero de columna a cambiar");
+        int columna = in.nextInt();
+        System.out.println("Ingrese el color a cambiar");
+        String color = in.nextLine();
+    }
+
+    /*Codigo para calcular el tiempo de juego
+      long startTime = System.currentTimeMillis();
+      long endTime = System.currentTimeMillis();
+      long elapsedTime = endTime - startTime;
+      System.out.println("Tiempo transcurrido: " + elapsedTime + " milisegundos");}
+     */
     
-    public static int pedirEntero(String mensaje, int minimo, int maximo) {
-      Scanner in = new Scanner(System.in);
-      int dato = 0;
-      boolean ok = false;
-
-      while(!ok) {
-         try {
-            System.out.print(mensaje);
-            dato = in.nextInt();
-            in.nextLine();
-            if (dato >= minimo && dato <= maximo) {
-               ok = true;
-            } else {
-               System.out.println("Valor fuera de rango (" + minimo + "-" + maximo + ")");
-            }
-         } catch (InputMismatchException var7) {
-            System.out.println("Ingrese solo numeros");
-            in.nextLine();
-         }
-      }
-
-      return dato;
-   }
 }
-
--- src/obligatorio1/TableroAzar.java
+++ src/obligatorio1/TableroAzar.java
@@ -7,65 +7,21 @@ public class TableroAzar {
     private int filas;
     private int columnas;
     private int dificultad;
-    private int solucion;
+    private String solucion;
     private String[][] tablero;
     
-    // Método para generar un tablero aleatorio
-//    public static char[][] generarTablero(int filas, int columnas, int nivel) {
-//        char[][] tablero = new char[filas][columnas];
-//        Random random = new Random();
-//        String[][] predefinido = {{"|A","-R","-R","\\R"},{"|A","/A","-R","-R","\\R"},{"-R","/A","|A","|R","/R"},{"/A","|A","-R","\\R","|A"},{"-R","-R","-R","|R","\\A"} };
-//        // Inicializar el tablero con símbolos aleatorios
-//        for (int i = 0; i < filas; i++) {
-//            for (int j = 0; j < columnas; j++) {
-//                tablero[i][j] = obtenerSimboloAleatorio(random);
-//            }
-//        }
-//
-//        // Resolver el tablero para que sea resoluble en 'nivel' pasos
-//        resolverTablero(tablero, nivel);
-//
-//        return tablero;
-//    }
-
-    // Método para obtener un símbolo aleatorio (/,\,-,|) con color aleatorio (R o A)
-    private static char obtenerSimboloAleatorio(Random random) {
-        char[] simbolos = {'/', '\\', '-', '|'};
-        char[] colores = {'R', 'A'};
-
-        char simbolo = simbolos[random.nextInt(simbolos.length)];
-        char color = colores[random.nextInt(colores.length)];
-
-        return simbolo == '/' || simbolo == '\\' ? color : simbolo;
+    //Asignar el tablero predeterminado:
+    public TableroAzar(){
+        this.filas = 5;
+        this.columnas = 6;
+        this.dificultad = 3;
+        this.tablero = new String[][] { {"|A" , "-R" , "-R" , "\\R" , "\\R"} , {"|A" , "/A" , "-R" , "-R" , "/R"} , {"-R" , "/A" , "|A" , "|R" , "/R"} , {"/A" , "|A" , "-R" , "\\R" , "|A"} , {"|R" , "-R" , "/R" , "|A" , "/A"} , {"-R" , "-R" , "-R" , "|R" , "\\A"} };
+        this.solucion = "(4,4) (5,6) (5,4)";
     }
-
+    
     // Método para resolver el tablero en un número determinado de pasos
-    private static void resolverTablero(char[][] tablero, int pasos) {
-        // Implementa la lógica para hacer el tablero resoluble en 'pasos' pasos
-        // Puedes implementar algún algoritmo para cambiar aleatoriamente ciertos símbolos
-        // mientras mantienes el tablero resoluble
-        // ...
-    }
-
-    // Método para imprimir el tablero en la consola
-    public static void imprimirTablero(String[][] tablero) {
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                System.out.print(tablero[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        int filas = 5;
-        int columnas = 5;
-        int nivel = 3;
-
-        String[][] tablero = generarTablero(filas, columnas, nivel);
-
-        System.out.println("Tablero generado:");
-        imprimirTablero(tablero);
+    private static String resolverTablero() {
+        return "0";
     }
     
     public TableroAzar(int fil , int col , int dif){
@@ -73,6 +29,7 @@ public class TableroAzar {
         this.columnas = col;
         this.dificultad = dif;
         this.tablero = generarTablero(fil,col,dif);
+        this.solucion = resolverTablero();
     }
     public static String[][] generarTablero(int fila , int column , int dificul){
         String[][] tablero = new String[fila][column];
@@ -88,5 +45,44 @@ public class TableroAzar {
         }
         return tablero;
     }
+    
+    public String[][] coloresTablero(){
+        String[] cols = {"\u001B[31m" , "\u001B[34m","\u001B[0m"};
+        String[][] ret = this.tablero;
+        for (int i = 0; i < this.tablero.length; i++) {
+            for (int j = 0; j < this.tablero[0].length; j++) {
+                if(this.tablero[i][j].charAt(1) == 'R'){
+                    this.tablero[i][j] = cols[0] + ret[i][j].charAt(0) + cols[2];
+                }else{
+                    this.tablero[i][j] = cols[1] + ret[i][j].charAt(0) + cols[2];
+                }
+            }
+        }
+        return ret;
+    }
+   
+    @Override
+    public String toString() {
+        String[][] conCol = this.coloresTablero();
+        String divHoriz = "  ";
+        String ret = "  ";
+        for (int i = 0; i < this.columnas; i++) {
+            int indice = i+1;
+            ret += "  " + indice + " ";
+            divHoriz += "+---";
+        }
+        divHoriz += "+";
+        for (int i = 0; i < this.columnas; i++) {
+            int indice = i+1;
+            ret += "\n" + divHoriz + "\n" + indice + " " ;
+            for (int j = 0; j < this.filas; j++) {
+                ret+= "|" + " " + conCol[i][j] + " ";
+            }
+            ret += "|";
+        }
+            ret += "\n" + divHoriz;
+        return ret.toString();
+    }
 }
+
 
